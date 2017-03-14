e12e3ca6271e2f26b78c8bda78fcc6237d6322ce
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-03-14 17:28:17 +0100

    add coroutine and deleting option

--- microapp/app/views.py
+++ microapp/app/views.py
@@ -1,5 +1,6 @@
 from app import app, models, db
 from flask import render_template, request, redirect, url_for
+import asyncio
 
 
 @app.route('/')
@@ -18,7 +19,35 @@ def add():
 def adding():
     nickname = request.args.get('nickname')
     text = request.args.get('text')
-    post = models.Post(nickname=nickname, text=text)
+    if text != "" and nickname != "":
+        post = models.Post(nickname=nickname, text=text)
+
+        # we create the asyncio loop
+        loop = asyncio.new_event_loop()
+
+        # Blocking call which returns when the display_date() coroutine is done
+        loop.run_until_complete(persist(post))
+        loop.close()
+    return redirect(url_for('index'))
+
+
+@app.route('/clean')
+def clean():
+    posts = models.Post.query.all()
+    loop = asyncio.new_event_loop()
+    for post in posts:
+        loop.run_until_complete(delete(post))
+    loop.close()
+    return redirect(url_for('index'))
+
+
+@asyncio.coroutine
+def delete(post):
+    db.session.delete(post)
+    db.session.commit()
+
+
+@asyncio.coroutine
+def persist(post):
     db.session.add(post)
     db.session.commit()
-    return redirect(url_for('index'))
