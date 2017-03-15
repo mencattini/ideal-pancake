a37caf94c2b8f9cd3aef8cf1801d0b3a7931a243
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-03-15 16:23:40 +0100

    add gitignore

--- microapp/app/views.py
+++ microapp/app/views.py
@@ -7,7 +7,7 @@ import asyncio
 @app.route('/index')
 def index():
     posts = models.Post.query.all()
-    return render_template('index.html', title='Nichon', posts=posts)
+    return render_template('index.html', title='MicroBlog', posts=posts)
 
 
 @app.route('/add')
@@ -17,8 +17,8 @@ def add():
 
 @app.route('/adding')
 def adding():
-    nickname = request.args.get('nickname')
-    text = request.args.get('text')
+    nickname = request.args.get('nickname')[0:64]
+    text = request.args.get('text')[0:140]
     if text != "" and nickname != "":
         post = models.Post(nickname=nickname, text=text)
 
@@ -41,13 +41,11 @@ def clean():
     return redirect(url_for('index'))
 
 
-@asyncio.coroutine
-def delete(post):
+async def delete(post):
     db.session.delete(post)
     db.session.commit()
 
 
-@asyncio.coroutine
-def persist(post):
+async def persist(post):
     db.session.add(post)
     db.session.commit()
