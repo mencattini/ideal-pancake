0d83cec5c2695e773b330beb2b8ef6b57cc02a3d
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-03-21 15:11:46 +0100

    add ast folder with learning programming ast

---
+++ AST/learn_ast.py
@@ -0,0 +1,24 @@
+import ast
+
+a = """
+
+@app.route('/adding')
+def adding():
+    nickname = request.args.get('nickname')
+    text = request.args.get('text')
+    if text and nickname:
+        nickname = nickname[0:64]
+        future = [persist_post(nickname, text) for text in wrap(text, 140)]
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(asyncio.wait(future))
+    return redirect(url_for('index'))
+
+async def persist_post(nickname, text):
+    post = models.Post(nickname=nickname, text=text)
+    await asyncio.sleep(1)
+    db.session.add(post)
+    db.session.commit()
+
+"""
+tree = ast.pase(a)
+print(ast.dump(tree))
