6d72e55e169754e24ef975a877781792d525d2cf
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-03-23 13:50:20 +0100

    modification liste, add boolean and natural from stew

---
+++ AST/bool.py
@@ -0,0 +1,41 @@
+from stew.core import Sort, generator, operation
+
+
+class Bool(Sort):
+
+    @generator
+    def true() -> Bool: pass
+
+    @generator
+    def false() -> Bool: pass
+
+    @operation
+    def __invert__(self: Bool) -> Bool:
+        if self == Bool.true():
+            return Bool.false()
+        return Bool.true()
+
+    @operation
+    def __and__(self: Bool, other: Bool) -> Bool:
+        if (self == Bool.true()) and (other == Bool.true()):
+            return Bool.true()
+        return Bool.false()
+
+    @operation
+    def __or__(self: Bool, other: Bool) -> Bool:
+        if self == Bool.true():
+            return Bool.true()
+        if other == Bool.true():
+            return Bool.true()
+        return Bool.false()
+
+    @operation
+    def __xor__(self: Bool, other: Bool) -> Bool:
+        if (self == Bool.true()) and (other == Bool.false()):
+            return Bool.true()
+        if (self == Bool.false()) and (other == Bool.true()):
+            return Bool.true()
+        return Bool.false()
+
+    def __bool__(self):
+        return self == Bool.true()
--- AST/liste.py
+++ AST/liste.py
@@ -1,11 +1,14 @@
 from stew.core import Sort, generator, operation
+from stew.matching import var
+from collections.abc import Sequence
 
 
 class Liste(Sort):
 
     def __init__(self, *args, **kwargs):
-        if(len(args) == 1) and isinstance(args[0], list):
+        if(len(args) == 1) and isinstance(args[0], Sequence):
             Sort.__init__(self)
+
             if len(args[0]) == 0:
                 self._generator = Liste.empty
             else:
@@ -21,3 +24,13 @@ class Liste(Sort):
     @generator
     def add(self: Liste, other: Sort) -> Liste:
         pass
+
+    @operation
+    def append(self: Liste, other: Liste) -> Liste:
+        # x + [] = x
+        if other == Liste.empty():
+            return self
+
+        #  x + (y,c) = add(x + y, c)
+        if other == Liste.add(var.y, var.c):
+            return Liste.add(self.append(var.y), var.c)
