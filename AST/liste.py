d1c47176df77371cb534cb5226ec53904479969f
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-03-28 22:41:30 +0200

    add the append method

--- AST/liste.py
+++ AST/liste.py
@@ -37,6 +37,11 @@ class List(Sort):
         if head == List.cons(tail=var.y, head=var.c):
             return List.cons(tail=tail + var.y, head=var.c)
 
+    @operation
+    def append(self: List, other: List) -> List:
+        self = self + other
+        return self
+
     def _as_list(self):
         if self._generator == List.empty:
             return ''
