33d76c896461c5fb9062ca97da3f8d7d94e626d9
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-03-28 18:06:07 +0200

    add working __add__ for list

--- AST/liste.py
+++ AST/liste.py
@@ -3,34 +3,36 @@ from stew.matching import var
 from collections.abc import Sequence
 
 
-class Liste(Sort):
+class List(Sort):
 
     def __init__(self, *args, **kwargs):
         if(len(args) == 1) and isinstance(args[0], Sequence):
             Sort.__init__(self)
 
             if len(args[0]) == 0:
-                self._generator = Liste.empty
+                self._generator = List.empty
             else:
-                self._generator = Liste.cons
-                self._generator_args = {'self': Liste(args[0][:-1]), 'other': args[0][-1]}
+                self._generator = List.cons
+                self._generator_args = {'tail': List(args[0][:-1]), 'head': args[0][-1]}
         else:
             Sort.__init__(self, **kwargs)
 
     @generator
-    def empty() -> Liste:
+    def empty() -> List:
         pass
 
     @generator
-    def cons(self: Liste, other: Sort) -> Liste:
+    def cons(tail: List, head: Sort) -> List:
         pass
 
     @operation
-    def append(self: Liste, other: Liste) -> Liste:
+    def __add__(tail: List, head: List) -> List:
         # x + [] = x
-        if other == Liste.empty():
-            return self
-
+        if head == List.empty():
+            return tail
+        # [] + y = y
+        if tail == List.empty():
+            return head
         #  x + (y,c) = cons(x + y, c)
-        if other == Liste.cons(var.y, var.c):
-            return Liste.cons(self.append(var.y), var.c)
+        if head == List.cons(tail=var.y, head=var.c):
+            return List.cons(tail=tail + var.y, head=var.c)
