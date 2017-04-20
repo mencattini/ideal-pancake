512c3175d490cb6fe3e436940f1dfb50881d5e8b
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-04-20 16:12:21 +0200

    add adt string and test for them

---
+++ ADT/tests/string_test.py
@@ -0,0 +1,27 @@
+from ADT.types.string import String
+from ADT.types.char import Char
+
+
+def test_generator():
+    assert String.empty()._generator == String.empty
+    assert String.cons(tail=String.empty(), head=Char('a'))._generator == String.cons
+
+
+def test_constructor():
+    assert String('') == String.empty()
+    assert String('ab') == String.cons(
+        tail=String.cons(tail=String.empty(), head=Char('a')),
+        head=Char('b'))
+
+
+def test_add_():
+    assert String('a') + String('b') == String('ab')
+    assert String('a') + String('') == String('a')
+    assert String('') + String('a') == String('a')
+    assert String('') + String('') == String('')
+
+
+def test_str_():
+    assert String('').__str__() == 'String()'
+    assert String.empty().__str__() == 'String()'
+    assert String('abc').__str__() == 'String(abc)'
---
+++ ADT/types/string.py
@@ -0,0 +1,50 @@
+from ADT.types.char import Char
+from stew.core import Sort, generator, operation
+from collections.abc import Sequence
+from stew.matching import var
+
+
+class String(Sort):
+    def __init__(self, *args, **kwargs):
+        if(len(args) == 1) and isinstance(args[0], Sequence):
+            Sort.__init__(self)
+
+            if len(args[0]) == 0:
+                self._generator = String.empty
+            else:
+                self._generator = String.cons
+                self._generator_args = {'tail': String(args[0][:-1]), 'head': Char((args[0][-1]))}
+        else:
+            Sort.__init__(self, **kwargs)
+
+    @generator
+    def empty() -> String:
+        pass
+
+    @generator
+    def cons(tail: String, head: Char) -> String:
+        pass
+
+    @operation
+    def __add__(tail: String, head: String) -> String:
+        # x + [] = x
+        if head == String.empty():
+            return tail
+        # [] + y = y
+        if tail == String.empty():
+            return head
+        #  x + (y,c) = cons(x + y, c)
+        if head == String.cons(tail=var.y, head=var.c):
+            return String.cons(tail=tail + var.y, head=var.c)
+
+    def _as_list(self):
+        if self._generator == String.empty:
+            return ''
+        elif self._generator == String.cons:
+            if self._generator_args['tail'] == String.empty():
+                return str(self._generator_args['head']._as_char())
+            else:
+                return self._generator_args['tail']._as_list() + self._generator_args['head']._as_char()
+
+    def __str__(self):
+        return '%s(%s)' % (self.__class__.__name__, self._as_list())
