0fe7cfe9ed3ac473b128047a68609b8af28eb588
dducry <ducry_d@hotmail.fr>
2017-04-22 21:56:33 +0200

    add of generators for Instr

--- ADT/types/instr.py
+++ ADT/types/instr.py
@@ -1,6 +1,7 @@
 from stew.core import Sort, generator, operation
 
 from ADT.types.expr import Expr
+from ADT.types.expr_list import Expr_list
 from ADT.types.bool import Bool
 from ADT.types.string import String
 from ADT.types.block import Block
@@ -14,7 +15,31 @@ class Instr(Sort):
         def i_assign(varName: String, expr: Expr) -> Instr:
             pass
 
-    
+    # While statement
     @generator
         def i_while(cond: Bool, block: Block) -> Instr:
             pass
+
+    # Specifies that we end a while statement
+    @generator
+        def i_end_while() -> Instr:
+            pass
+
+    # If statement
+    @generator
+        def i_if(cond: Bool, b_then: Block, b_else: Block) -> Instr:
+            pass
+
+    # Specifies that we end an if statement
+    @generator
+        def i_end_if() -> Instr:
+            pass
+
+    # Function (procedure) to be executed (we don't care about the returned value if exists)
+    @generator
+        def i_func(e_list: Expr_List) -> Instr:
+            pass
+
+
+
+
