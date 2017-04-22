27a70abbe9e1fd9fbc2e153e37ab551a377259e2
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-04-22 23:49:43 +0200

    comment all the context.py, must be change

--- ADT/types/context.py
+++ ADT/types/context.py
@@ -1,41 +1,36 @@
 from stew.core import Sort, generator, operation
-
+from stew.matching import var
 from ADT.types.bool import Bool
 from ADT.types.string import String
 
 
-
-''' Context sort represents the list of all substitutions for a specific context'''
 class Context(Sort):
+    """ Context sort represents the list of all substitutions for a specific context"""
+    pass
+    # @generator
+    # def empty() -> Context:
+    #     pass
 
-    @generator
-    def empty() -> Context:
-        pass
+    # @generator
+    # def add(my_context: Context, key: String, value: Expr) -> Context:
+    #     return Context.add(remove_key(my_context), key, value)
 
-    @generator
-    def add(my_context: Context, key: String, value: Expr) -> Context:
-        return add(remove_key(my_context), key, value)
+    # @operation
+    # def contains_key(my_context: Context, key: String) -> Bool:
+    #     if my_context == Context.empty():
+    #         return Bool.false()
 
+    #     if my_context == Context.add(var.c, var.k, var.e):
+    #         if key == var.k:
+    #             return Bool.true()
+    #         return contains_key(var.c, key)
+    #
+    # @operation
+    # def remove_key(my_context: Context, key: String) -> Context:
+    #     if my_context == Context.empty():
+    #         return my_context
 
-    @operation
-    def contains_key(my_context: Context, key: String) -> Bool:
-        if my_context == Context.empty():
-            return Bool.false()
-        
-        if my_context == Context.add(var.c, var.k, var.e):
-            if key == var.k:
-                return Bool.true()
-            return contains_key(var.c, key)
-
-
-    @operation
-    def remove_key(my_context: Context, key: String) -> Context:
-        if my_context == Context.empty():
-            return my_context
-
-        if my_context == Context.add(var.c, var.k, var.e):
-            if key == var.k:
-                return remove_key(var.c, key)
-            return add(remove_key(var.c, key), var.k, var.e)
-    
-
+    #     if my_context == Context.add(var.c, var.k, var.e):
+    #         if key == var.k:
+    #             return remove_key(var.c, key)
+    #         return add(remove_key(var.c, key), var.k, var.e)
