24d544ce5840ec09cd883e64ee2abe9dc9338cec
Romain Mencattini <romain.mencattini@etu.unige.ch>
2017-04-23 20:02:41 +0200

    correction of context_list, adding some tests

---
+++ ADT/tests/context_list_test.py
@@ -0,0 +1,44 @@
+from ADT.types.context_list import Context, Context_list
+from ADT.types.string import String
+from ADT.types.expr import Expr
+from ADT.types.nat import Nat, Bool
+
+
+def test_generator():
+    assert Context_list.empty()._generator == Context_list.empty
+
+    a = Context({'a': Bool.true()})
+    assert Context_list.cons(
+        tail=Context_list.empty(),
+        head=a)._generator == Context_list.cons
+
+
+def test_constructor():
+    assert Context_list([]) == Context_list.empty()
+
+    a = Context({'a': Bool.true()})
+    b = Context({'b': Nat(10)})
+
+    assert Context_list.cons(
+        tail=Context_list.cons(
+            tail=Context_list.empty(),
+            head=a),
+        head=b) == Context_list([a, b])
+
+
+def test_car():
+    assert Context_list([]).car() == Context.empty()
+
+    a = Context({'a': Bool.true()})
+    b = Context({'b': Nat(10)})
+
+    assert Context_list([a, b]).car() == b
+
+
+def test_cdr():
+    assert Context_list([]).car() == Context.empty()
+
+    a = Context({'a': Bool.true()})
+    b = Context({'b': Nat(10)})
+
+    assert Context_list([a, b]).cdr() == Context_list([a])
--- ADT/types/context_list.py
+++ ADT/types/context_list.py
@@ -1,11 +1,23 @@
 from stew.core import Sort, generator, operation
-from stew.matching import var
-
+from collections.abc import Sequence
+from ADT.types.bool import Bool
 from ADT.types.context import Context
 
 
 class Context_list(Sort):
     """ Context_list sort represents the stack of contexts"""
+
+    def __init__(self, *args, **kwargs):
+        if (len(args) == 1) and isinstance(args[0], Sequence):
+            Sort.__init__(self)
+
+            if len(args[0]) == 0:
+                self._generator = Context_list.empty
+            else:
+                self._generator = Context_list.cons
+                self._generator_args = {'tail': Context_list(args[0][:-1]), 'head': args[0][-1]}
+        else:
+            Sort.__init__(self, **kwargs)
 
     @generator
     def empty() -> Context_list:
@@ -17,14 +29,40 @@ class Context_list(Sort):
 
     @operation
     def car(c_list: Context_list) -> Context:
-        if c_list == Context_list.cons(var.t, var.h):
-            return var.h
-        # No precision about car(Empty)
-        pass
+        if c_list._generator == Context_list.cons:
+            return c_list._generator_args['head']
+        else:
+            return Context.empty()
 
     @operation
     def cdr(c_list: Context_list) -> Context_list:
-        if c_list == Context_list.cons(var.t, var.h):
-            return var.t
-        # No precision about cdr(Empty)
-        pass
+        if c_list._generator == Context_list.cons:
+            return c_list._generator_args['tail']
+        else:
+            return Context_list.empty()
+
+    @operation
+    def __eq__(self: Context_list, other: Context_list) -> Bool:
+        # if it's two empty expr list they are equal
+        if self._generator == Context_list.empty and other._generator == self._generator:
+            return Bool.true()
+        # if a generator is empty and the other is cons
+        elif not(self._generator == other._generator):
+            return Bool.false()
+        # if the generators are equal we continue
+        elif self._generator_args['head'] == other._generator_args['head']:
+            return self._generator_args['tail'].__eq__(other._generator_args['tail'])
+        # else we stop
+        else:
+            return Bool.false()
+
+    def _as_list(self):
+        if self._generator == Context_list.empty:
+            return ''
+        elif self._generator == Context_list.cons:
+            if self._generator_args['tail']._generator == Context_list.empty:
+                return self._generator_args['head'].__str__()
+            return self._generator_args['tail']._as_list() + ',' + self._generator_args['head'].__str__()
+
+    def __str__(self):
+        return '%s(%s)' % (self.__class__.__name__, self._as_list())
